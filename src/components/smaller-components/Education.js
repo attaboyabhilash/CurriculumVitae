d6c1c7eda2284e8e5b97b2555679bd42fbdca706
Abhilash Negi <tiger.abhi.3609@gmail.com>
2020-11-09 22:17:24 +0530

    commiting after deploy

--- src/components/smaller-components/Education.js
+++ src/components/smaller-components/Education.js
@@ -5,14 +5,17 @@ function Education() {
         <div className="small-container right">
             <h2>Education Details</h2>
             <ul>
-                <li>St. Joseph’s Degree and PG College - &nbsp; &nbsp; &nbsp;April 2018<br/>                                                 	              			             
+                <li>St. Joseph’s Degree and PG College - &nbsp;April 2018<br/>                                                	              			             
                     <small>BSc in Computer Science - 94.1%</small>
+                    <hr /> 
                 </li>
-                <li>Rashtriya Military School, Chail - &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;April 2015<br/>                                           	              			            
+                <li>Rashtriya Military School, Chail - &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;April 2015<br/>                                           	              			            
                     <small>12th Standard - 69.6%</small>
+                    <hr />
                 </li>
-                <li>Rashtriya Military School, Chail - &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;April 2013<br/>                                                	              			             
+                <li>Rashtriya Military School, Chail - &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;April 2013<br/>                                                	              			             
                     <small>10th Standard - 9.0 GPA</small>
+                    <hr />
                 </li>
             </ul>
         </div>
--- src/components/smaller-components/Trainings.js
+++ src/components/smaller-components/Trainings.js
@@ -2,12 +2,15 @@ import React from 'react'
 
 function Trainings() {
     return (
-        <div className="small-container">
+        <div className="small-container right">
             <h2>Trainings</h2>
             <ul>
                 <li>Modern Javascript ES6 and ES7 training by <span>Udemy</span></li>
+                <hr />
                 <li>Front End Libraries training by <span>FreeCodeCamp</span></li>
+                <hr />
                 <li>Front End Developer with ReactJS training by <span>Scrimba</span></li>
+                <hr />
             </ul>
         </div>
     )
